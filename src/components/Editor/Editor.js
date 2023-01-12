697cfc0c9ba37f020bf714bd275979ee8a3502a1
spiderforrest <spider@spood.org>
2023-01-11 16:31:35 -0800

    set title state

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './Editor.css';
 
-export default function Editor() {
+export default function Editor({ setTitle }) {
   return (
     <div className="editor">
       <div className="form-control">
-        <input name="title" type="text" />
+        <input name="title" type="text" onChange={(e) => setTitle(e.target.value)} />
         <label htmlFor="title">Title</label>
       </div>
       <div className="form-control">
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 import Preview from '../Preview/Preview';
 import Editor from '../Editor/Editor';
@@ -7,12 +8,13 @@ import './Home.css';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
+  const [title, setTitle] = useState('');
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview title={title} />
+      <Editor setTitle={setTitle} />
     </main>
   );
 }
